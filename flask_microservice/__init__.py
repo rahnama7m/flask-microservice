926cbb7b274faf1397250b46b2a7b8460f76314e
Mostafa Rahnama <m.rah1372@gmail.com>
2023-05-18 17:01:31 +0330

    Redesign microservice base module

--- flask_microservice/__init__.py
+++ flask_microservice/__init__.py
@@ -1,1 +1,10 @@
-from flask_microservice.microservice import create_app
+"""
+# Flask Microservice Project-  API Version 1
+## Architecture
+In this project we use MVC architecture. Components of each section are:
+    1. Model: In root of application represented by models/app_models.py
+    2. View: In each version of project (Exp: api/v1),  represented by resource.py
+    3. Controller: In each version of project (Exp: api/v1), represented by controller.py
+
+"""
+from .microservice import create_app
--- flask_microservice/microservice.py
+++ flask_microservice/microservice.py
@@ -5,10 +5,12 @@
 from flask import Flask
 
 # Import project packages and modules
-from .settings.config import Config
+from .settings import config
+from .api.v1 import resource as api_v1_resource
 
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config.Config)
+    app.register_blueprint(api_v1_resource.api_v1_blp)
     return app
