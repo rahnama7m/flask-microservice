a43a398b7d5748ab98b8371df5d5b50f29d5f7a7
Mostafa Rahnama <m.rah1372@gmail.com>
2023-05-18 17:00:53 +0330

    Structure- Define api v1  controllers and resoures

---
+++ flask_microservice/api/v1/controller/task.py
@@ -0,0 +1,30 @@
+# Import Python packages and modules
+
+
+# Import Flask packages and modules
+
+# Import project packages and modules
+from flask_microservice.utils import json_response_parser
+
+
+class TaskController:
+
+    @staticmethod
+    def get_tasks():
+        return json_response_parser(status=503, internal_code=501)
+
+    @staticmethod
+    def get_task(task_id):
+        return json_response_parser(status=501, internal_code=501)
+
+    @staticmethod
+    def create_task():
+        return json_response_parser(status=501, internal_code=501)
+
+    @staticmethod
+    def update_task(task_id):
+        return json_response_parser(status=501, internal_code=501)
+
+    @staticmethod
+    def delete_task(task_id):
+        return json_response_parser(status=501, internal_code=501)
