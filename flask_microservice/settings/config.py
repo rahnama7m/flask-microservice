63cccd3cb068678610a9981cfa8397bd9ae0ff4d
Mostafa Rahnama <m.rah1372@gmail.com>
2023-05-27 22:44:46 +0330

    Add SQLAlchemy and Migrate object to app, run migrate commands

--- flask_microservice/settings/config.py
+++ flask_microservice/settings/config.py
@@ -8,3 +8,16 @@ load_dotenv(find_dotenv())
 class Config:
     TESTING = DEBUG = bool(int(os.environ.get("DEBUG", "0")))
     ENV = os.environ.get("ENVIRONMENT", "production")
+
+    DATABASE_ENGINE = os.environ.get("DATABASE_ENGINE", None)
+    DATABASE_NAME = os.environ.get("DATABASE_NAME", None)
+    DATABASE_USER = os.environ.get("DATABASE_USER", None)
+    DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD", None)
+    DATABASE_HOST = os.environ.get("DATABASE_HOST", None)
+    DATABASE_PORT = os.environ.get("DATABASE_PORT", None)
+
+    SQLALCHEMY_DATABASE_URI = f"{DATABASE_ENGINE}://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+
+    SQLALCHEMY_ECHO = DEBUG
+    SQLALCHEMY_RECORD_QUERIES = DEBUG
+    SQLALCHEMY_TRACK_MODIFICATIONS = DEBUG
